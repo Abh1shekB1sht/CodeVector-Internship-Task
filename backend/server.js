119ade25c2772ffc4ebb47ad2fdeca248787a888
Abhishek <singhbisht84484@gmail.com>
2026-06-26 18:43:05 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: /https:\/\/code-vector-internship-task.*\.vercel\.app$/,
+    origin: "https://code-vector-internship-task-1kz5.vercel.app",
   }),
 );
 
